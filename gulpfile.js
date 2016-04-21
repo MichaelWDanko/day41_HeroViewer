39c0a5e515f451d7702cf98eaac80b754081b31b
Michael Danko <MichaelDanko@icloud.com>
2016-04-20 23:36:54 -0400

    Have list display rendering a list. List items link to the detail page.

--- gulpfile.js
+++ gulpfile.js
@@ -5,8 +5,10 @@ var browserify = require('gulp-browserify');
 gulp.task('default', ['html', 'js', 'css']);
 
 gulp.task('html', function () {
-    return gulp.src('./*.html')
+    gulp.src('./*.html')
         .pipe(gulp.dest('./public'));
+    return gulp.src('./sections/*.html')
+        .pipe(gulp.dest('./public/sections'));
 });
 
 gulp.task('css', function () {
@@ -16,13 +18,14 @@ gulp.task('css', function () {
 });
 
 gulp.task('js', function () {
-  return gulp.src('./js/app.js')
+    return gulp.src('./js/app.js')
         .pipe(browserify())
         .pipe(gulp.dest('./public/js'));
 });
 
 gulp.task('watch', function () {
     gulp.watch('./*.html', ['html']);
+    gulp.watch('./sections/*.html', ['html']);
     gulp.watch('./scss/*.scss', ['css']);
     gulp.watch('./js/*.js', ['js']);
     gulp.watch('./js/models/*.js', ['js']);
--- js/app.js
+++ js/app.js
@@ -1,40 +1,122 @@
 /* jslint browser: true */
 /* jslint esnext: true */
 var angular = require('angular');
-var mainApp = angular.module('MarvelSuperHeroApp', []);
+var angularRoute = require('angular-route');
+var app = angular.module('MarvelSuperHeroApp', ['ngRoute']);
 
-mainApp.controller('MainController', function ($scope, $http) {
-    $scope.name = 'Mike';
-    $scope.heroes = [];
-    $scope.current = {
+//app.controller('ListViewController2', function ($scope, $http) {
+//    //    $scope.name = 'Mike';
+//    //    $scope.heroes = [];
+//    //    $scope.current = {
+//    //        name: '',
+//    //        url: '',
+//    //        id: '',
+//    //    };
+//    //    $scope.giraffe = '';
+//    //    $scope.events = [];
+//    //    $scope.getDetails = function (hero) {
+//    //        console.log('Clicked on ' + hero.name + ' who has the ID: ' + hero.id);
+//    //        $scope.current.name = hero.name;
+//    //        $scope.current.url = (hero.thumbnail.path + '/landscape_incredible.' + hero.thumbnail.extension);
+//    //        $scope.current.id = hero.id;
+//    //        $http({
+//    //            method: 'get',
+//    //            url: "http://gateway.marvel.com:80/v1/public/characters/" + hero.id + "/events?orderBy=-name&limit=5&apikey=0e7466623241b56d92fc74e4d5039354",
+//    //        }).then(function (response) {
+//    //           $scope.events = response.data.data.results;
+//    //            console.log('events =');
+//    //            console.log($scope.events);
+//    //            return response;
+//    //        });
+//    //    };
+//
+//    //    $http({
+//    //        method: 'get',
+//    //        url: 'https://gateway.marvel.com:443/v1/public/characters?limit=100&offset=500&apikey=0e7466623241b56d92fc74e4d5039354',
+//    //    }).then(function (response) {
+//    //        console.log(response.data.data.results);
+//    //        $scope.heroes = response.data.data.results;
+//    //    });
+//});
+
+var angular = require('angular');
+var angularRoute = require('angular-route');
+var app = angular.module('MarvelSuperHeroApp', ['ngRoute']);
+
+
+/*The AngularJS router below.*/
+app.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider
+        .when('/list', {
+            controller: 'ListViewController',
+            templateUrl: 'sections/list-view.html',
+            resolve: 'ListViewController.resolve'
+        })
+        .when('/detail', {
+            controller: 'DetailViewController',
+            templateUrl: 'sections/detail-view.html',
+        });
+}]);
+
+
+/*The view for the general list of heroes*/
+app.controller('ListViewController', ['$scope', '$http', 'CurrentHero', '$route', function ($scope, $http, CurrentHero, $route) {
+
+    console.log('ListView Switched');
+
+    $scope.heroes = CurrentHero.getHeroes();
+    $scope.getDetails = function (hero) {
+        console.log("clicked on "+ hero.name);
+       CurrentHero.setCurrent(hero);
+        
+    }
+    $route.updateParams('detail');
+}]);
+
+/*The view for the specifc list of a heroes' events*/
+app.controller('DetailViewController', ['$scope', '$http', 'CurrentHero', function ($scope, $http, CurrentHero) {
+    console.log('DetailView Switched');
+
+}]);
+
+/*The factory to create the CurrentHero service.*/
+app.factory('CurrentHero', function ($http, $route) {
+    var name = '';
+    var heroes = [];
+    var current = {
         name: '',
         url: '',
         id: '',
     };
-    $scope.giraffe = '';
-    $scope.events = [];
-    $scope.getDetails = function (hero) {
-        console.log('Clicked on ' + hero.name + ' who has the ID: ' + hero.id);
-        $scope.current.name = hero.name;
-        $scope.current.url = (hero.thumbnail.path + '/landscape_incredible.' + hero.thumbnail.extension);
-        $scope.current.id = hero.id;
-        $http({
-            method: 'get',
-            url: "http://gateway.marvel.com:80/v1/public/characters/" + hero.id + "/events?orderBy=-name&limit=5&apikey=0e7466623241b56d92fc74e4d5039354",
-        }).then(function (response) {
-           $scope.events = response.data.data.results;
-            console.log('events =');
-            console.log($scope.events);
-            return response;
-        });
-    };
+    var giraffe = '';
+    var events = [];
 
     $http({
         method: 'get',
-        url: 'https://gateway.marvel.com:443/v1/public/characters?limit=100&offset=500&apikey=0e7466623241b56d92fc74e4d5039354',
+        url: 'https://gateway.marvel.com:443/v1/public/characters?limit=50&offset=500&apikey=0e7466623241b56d92fc74e4d5039354',
     }).then(function (response) {
-        console.log(response.data.data.results);
-        $scope.heroes = response.data.data.results;
-    });
-});
+        console.log('Heroes array before');
+        console.log(heroes);
+        heroes = response.data.data.results;
+        console.log('Heroes array after');
+        console.log(heroes);
 
+//        console.log(response.data.data.results);
+    }).then(function () {
+       $route.reload();
+    });
+    /*Feels extremely hacky^^*/
+
+
+    return {
+        getHeroes: function () {
+            return heroes;
+        },
+        setCurrent: function (hero) {
+            current.name = hero.name;
+            current.id = hero.id;
+            current.url = (hero.thumbnail.path + '/landscape_incredible.' + hero.thumbnail.extension)
+        }
+    };
+
+});
